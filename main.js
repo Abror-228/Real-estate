bab5c91cd3f051acce89d421c23fea5e750f3e57
Abror <abrorakaunt99999@gmail.com>
2026-05-23 18:07:36 +0500

    af

---
+++ main.js
@@ -0,0 +1,9 @@
+var elSpinnerWrap = document.querySelector(".spinner-wrapper");
+
+window.addEventListener("load", () => {
+    elSpinnerWrap.style.opacity = "1";
+
+    setTimeout(() => {
+        elSpinnerWrap.style.display = "none";
+    }, 1670)
+});
